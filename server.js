3c560f087dcb172daeee57bd5075c23e02ae8907
Marshall An <haokanga@andrew.cmu.edu>
2020-07-22 23:04:14 -0400

    add first GET route

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const express = require('express');
 // The next creates an instance of the express constructor, which we will name "app".
 const app = express();
 
+
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
+
 // We can now access methods used for making a server by including their name after app.
-app.listen(8000,function(){
+app.listen(8000, function () {
     console.log("server is running")
 })
 // The app.listen method will start up the server locally on the port you give as its first argument
