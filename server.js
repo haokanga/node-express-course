3ac93c2af705dd8c6d3016267f6b3db8fa899aca
Marshall An <haokanga@andrew.cmu.edu>
2020-07-22 23:07:38 -0400

    add login POST route

--- server.js
+++ server.js
@@ -25,6 +25,27 @@ app.get('/users/:id', function (req, res) {
     })
 })
 
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
 // We can now access methods used for making a server by including their name after app.
 app.listen(8000, function () {
     console.log("server is running")
